b02d6a1fba692c3eb0906936d731dc8b09bbab14
Marcus Huang <marcuschuang@gmail.com>
2022-07-22 02:22:49 -0500

    First Commit wfa_service

---
+++ src/main/java/com/wfa/WfaServiceApplication.java
@@ -0,0 +1,13 @@
+package com.wfa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WfaServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WfaServiceApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/wfa/service/package-info.java
@@ -0,0 +1,1 @@
+package com.wfa.service;
